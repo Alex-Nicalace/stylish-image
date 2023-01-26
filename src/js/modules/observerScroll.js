6289c58029f52d2c595b96049b8fbd5bf41cb879
Alex-Nicalace <anicalace@gmail.com>
2023-01-26 09:16:07 +0200

    modified observer scroll

--- src/js/modules/observerScroll.js
+++ src/js/modules/observerScroll.js
@@ -10,7 +10,7 @@ const optionsDefault = {
    delay: 100              // minimum 100
 }
 
-function createIntersectionObserver(targetSelector, callback, options = {}) {
+function createIntersectionObserver(targetSelector, nameClass, options = {}) {
    const optionsCurrent = { ...optionsDefault, ...options };
 
    const observer = new IntersectionObserver(onIntersection, optionsCurrent);
@@ -22,32 +22,18 @@ function createIntersectionObserver(targetSelector, callback, options = {}) {
 
    function onIntersection(entries, observer) {
       for (const entry of entries) {
-         callback(entry, observer)
+         const target = entry.target;
+         if (entry.isIntersecting) {
+            target.classList.add(nameClass);
+            observer.unobserve(target);
+         }
       }
    };
 }
 
-function createFunc(blockSelector, nameClass) {
-   return (entry, observer) => {
-      const modifiedElements = document.querySelectorAll(blockSelector);
-      modifiedElements.forEach(element => {
-         if (entry.isIntersecting) {
-            element.classList.add(nameClass);
-         } else {
-            element.classList.remove(nameClass);
-         };
-      });
-   }
-}
 
-const animatePromo = createFunc('.promo__header-block', 'promo__header-block_animated');
-const animateAdvantage = createFunc('.advantage', 'advantage_animated');
-const animateComposition = createFunc('.composition', 'composition_animated');
-const animateSubscribe = createFunc('.subscribe', 'subscribe_animated');
-
-createIntersectionObserver('.promo__header-block', animatePromo, { rootMargin: '60px 0px 0px 0px' });
-createIntersectionObserver('.advantage', animateAdvantage, { rootMargin: '274px 0px' });
-createIntersectionObserver('.composition', animateComposition, { rootMargin: '450px 0px' });
-createIntersectionObserver('.subscribe', animateSubscribe, { rootMargin: '670px 0px' });
+createIntersectionObserver('.promo__header-block', 'promo__header-block_animated', { rootMargin: '0px 0px 0px 0px' });
+createIntersectionObserver('.advantage', 'advantage_animated', { rootMargin: '0px 0px', threshold: 0.5 });
+createIntersectionObserver('.subscribe', 'subscribe_animated', { rootMargin: '0px 0px', threshold: 0.5 });
 // </наблюдение за элементом при помощи Intersection Observer API>
 // </animate for visible element>
