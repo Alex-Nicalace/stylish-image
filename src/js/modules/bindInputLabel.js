0ff7b7c98b37b6b983c753947640ba043ce940d8
Alex-Nicalace <anicalace@gmail.com>
2023-02-05 14:01:17 +0200

    amend article-card

--- src/js/modules/bindInputLabel.js
+++ src/js/modules/bindInputLabel.js
@@ -2,7 +2,7 @@ const bindInputLabel = () => {
    const labels = document.querySelectorAll('input+label:not([for])');
    labels.forEach(label => {
       const input = label.previousElementSibling;
-      const id = input.id || `${input.name}${Date.now()}`;
+      const id = input.id || `${input.name}_${input.value}_${Date.now()}`;
       input.id = id;
       label.setAttribute('for', id);
    });
